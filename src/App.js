2f06040725a930f6bb76b29b131558645f0d8a7c
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-12-08 17:44:27 -0500

    [App] Init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Movie from './Movie';
+
+function App() {
+  return (
+    <div className="App">
+      <Movie title="The Fellowship of the Ring" hours="2" minutes="58"/>
+      <Movie title="The Two Towers" hours="2" minutes="59"/>
+      <Movie title="The Return of the King" hours="3" minutes="21"/>
+    </div>
+  );
+}
+
+export default App;
