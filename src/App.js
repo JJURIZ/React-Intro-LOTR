d4b44b9874bf1836fbc2428a7c444c8676bee978
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-12-09 09:36:24 -0500

    [SASS, App] Included map, style.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
 import Movie from './Movie';
+import './sass/style.scss';
+
+
+let movieData = [
+  {
+    title: "The Fellowship of the Ring",
+    hours: "2",
+    minutes: "58"
+  },
+  {
+    title: "The Two Towers",
+    hours: "2",
+    minutes: "59"
+  },
+  {
+    title: "Return of the King",
+    hours: "3",
+    minutes: "21"
+  }
+]
+
+const movies = movieData.map((components, idx) => {
+  return <Movie title={components.title} hours={components.hours} minutes={components.hours} key={idx} />
+});
+
 
 function App() {
   return (
     <div className="App">
-      <Movie title="The Fellowship of the Ring" hours="2" minutes="58"/>
-      <Movie title="The Two Towers" hours="2" minutes="59"/>
-      <Movie title="The Return of the King" hours="3" minutes="21"/>
+      {movies}
     </div>
   );
 }
